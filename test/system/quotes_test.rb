ce422a1410715a94008c8ac0082b85469111da6b
tsujiguchi <tsujiguchi@careerindex.co.jp>
2022-06-26 14:18:33 +0900

    turbo_stream追加

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = Quote.ordered.first
   end
 
   test "Creating a new quote" do
