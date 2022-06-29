e7d0debd287023bb53f8b150d5d45bf50f861170
tsujiguchi <tsujiguchi@careerindex.co.jp>
2022-06-29 19:42:31 +0900

    ActionCableを利用したリアルタイム更新

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,14 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # # partialとlocalsはデフォルト値がモデルから推測できるので、省略可
+  # # after_create_commit -> { broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quote: self } }
+  # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # # broadcast_remove_later_to はデータが削除されると取得してジョブを実行できないのでそのメソッドはない
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+
+  # after_create_commit after_update_commit after_destroy_commit は以下に纏められる
+  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
 end
