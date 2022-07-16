9947386671a91b448ad173d41996ad827e3d15cd
tsujiguchi <tsujiguchi@careerindex.co.jp>
2022-07-16 11:13:44 +0900

    Adding a quote total with Turbo Frames

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,16 +3,13 @@ class Quote < ApplicationRecord
 
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   scope :ordered, -> { order(id: :desc) }
 
-  # # partialとlocalsはデフォルト値がモデルから推測できるので、省略可
-  # # after_create_commit -> { broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quote: self } }
-  # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
-  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
-  # # broadcast_remove_later_to はデータが削除されると取得してジョブを実行できないのでそのメソッドはない
-  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
-
-  # after_create_commit after_update_commit after_destroy_commit は以下に纏められる
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
